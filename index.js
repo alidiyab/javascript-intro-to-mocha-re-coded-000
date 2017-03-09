4f518004e16f17c8bec0264fa9d604fbc044e6c3
Ali Diyab <alidiyab1997@gmail.com>
2017-03-09 15:46:55 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+	return true;
+}
+
+function sayHey(){
+	return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+	return `Hey, ${name}!`
+}
+
+function favoriteIceCream(name_of_icecream) {
+	return `I love ${name_of_icecream}`
+}
+
+function shouting(upper){
+return 'I LOVE JAVASCRIPT SOOOOOO MUCH'
+}
+
+function roundDown(num) {
+	return Math.floor(num);
+}
